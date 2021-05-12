f5c907780acc233773b37c7be7d3bbc3e2baec89
Remo <git@rwalser.ch>
2021-05-12 20:37:37 +0200

    Flask login module

--- webroot/auth.py
+++ webroot/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_required, login_user, logout_user, current_user
 
 auth = Blueprint('auth', '__name__')
 
@@ -15,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, passwd):
                 flash(f'Sali {user.first_name}, du bist angemeldet.', category='succsess')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Sorry, aber die stimmt was nicht.', category='error')
@@ -23,8 +25,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -53,7 +57,7 @@ def sign_up():
             ))
             db.session.add(new_user)
             db.session.commit()
-
+            login_user(user, remember=True)
             flash('Gratulation, du hast dich erfolgreich registiert.', category='success')
             return redirect(url_for('views.home'))
 
